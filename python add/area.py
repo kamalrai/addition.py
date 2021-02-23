e487f809e6bf4f59c875d8cb0ca01a8ed88de836
kamalrai <nahrai123@gmail.com>
2021-02-23 11:56:32 +0545

    printing area

---
+++ python add/area.py
@@ -0,0 +1,10 @@
+#write a program that reads the length of the base and the height of a right angled triangle and prints the area. Every
+number is given on a separate line.
+
+length = int(input("enter the length of the base of the triangle: "))
+height = int(input("enter the length of height of the triangle: "))
+
+area=(length*height)//2
+#the floor division // rounds the reasult down to the nearest whole number
+
+print(f"the area of the triangle is (area)")
---
+++ python add/name.py
@@ -0,0 +1,9 @@
+
+name = str(input("enter your name: "))
+age = str(input("enter your age: "))
+
+print(f"your name is {name} your age is{age})
+print("hello my name is" + name + " and i am " + str(age) + "years old)
+print("hello my name is%s and i am %d years old" % (name, age))
+print("hello my name is {} and i am {} years old".format(name, age))
+
